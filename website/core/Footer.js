12b3e48af7d55440f2a56870c5b35de24919bf48
Matteo Manzinello <matteo@manzinello.com>
2019-10-03 20:05:07 +0200

    other @netlify badge

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -78,7 +78,7 @@ class Footer extends React.Component {
             target="_blank"
             href="https://www.netlify.com"
           >
-            <img src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg" />
+            <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" />
           </a>
         </section>
       </footer>
